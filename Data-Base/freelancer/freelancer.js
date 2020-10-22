a5562ff496883a8133bde444d8a8faccdd931f69
ahmedallani <ahmed.allani@rbk.tn>
2020-10-22 22:38:48 +0100

    async

--- Data-Base/freelancer/freelancer.js
+++ Data-Base/freelancer/freelancer.js
@@ -1,33 +1,56 @@
-const {connection}=require("../database")
-var bcrypt = require('bcryptjs');
+const { connection } = require("../database");
+var bcrypt = require("bcryptjs");
 const { get } = require("../../ServerRoutes/Client");
 var salt = bcrypt.genSaltSync(10);
+const sql = (str) =>
+  new Promise((resolve, reject) => {
+    connection.query(str, function(error, results) {
+      if (error) {
+        reject(error);
+      }
+      resolve(results);
+    });
+  });
 
-const loginFreeLancer= (req,callback)=>{
-    var userData=null
-    var password=null
-       connection.query(`SELECT * from Freelancers where Email="${req.Email}"`, function (error, results, fields) {
-           if(results.length){
-               userData=results[0]
-               password=results[0].password
-               {bcrypt.compareSync(req.Password, password)?callback({error:null,userData}):callback({error:"Wrong Password",userData:null})}
-           }else{
-               callback({error:"Email Unvalid",userData:null,})
-           }
-         }); 
-  }
-
-
-const SignupFreeLancer= (req,callback)=>{
-    if(req.Password){
-      var hash = bcrypt.hashSync(req.Password, salt);
-      var query=`INSERT INTO Freelancers (FirstName,LastName,Email,password,Gender,Age,City,Adresse,Field) values ('${req.FirstName}','${req.LastName}','${req.Email}','${hash}','${req.Gender}',${req.Age},'${req.City}','${req.Adresse}','${req.Field}');`
-      connection.query(query, function (error, results, fields) {callback(results,error)});
+// _*_ {Email,Password} instand of req
+const loginFreeLancer = async (req) => {
+  var userData = null;
+  var password = null;
+  try {
+    let results = await sql(
+      `SELECT * from Freelancers where Email="${req.Email}"`
+    );
+    if (results.length) {
+      userData = results[0];
+      password = results[0].password;
+      let rtn=bcrypt.compareSync(req.Password, password)
+          ? { error: null, userData }
+          : { error: "Wrong Password", userData: null };
+      return rtn
+    } else {
+      return { error: "Email Unvalid", userData: null }
     }
-    ////dont forget this
-}
-
-  module.exports={
-    SignupFreeLancer,
-    loginFreeLancer
+  } catch (err) {
+    console.log(err);
   }
+};
+
+const SignupFreeLancer = (req, callback) => {
+  if (req.Password) {
+    var hash = bcrypt.hashSync(req.Password, salt);
+    var query = `INSERT INTO Freelancers (FirstName,LastName,Email,password,Gender,Age,City,Adresse,Field) values ('${
+      req.FirstName
+    }','${req.LastName}','${req.Email}','${hash}','${req.Gender}',${req.Age},'${
+      req.City
+    }','${req.Adresse}','${req.Field}');`;
+    connection.query(query, function(error, results, fields) {
+      callback(results, error);
+    });
+  }
+  ////dont forget this
+};
+
+module.exports = {
+  SignupFreeLancer,
+  loginFreeLancer,
+};
--- ServerRoutes/FreeLancer.js
+++ ServerRoutes/FreeLancer.js
@@ -1,38 +1,40 @@
-const express = require('express')
+const express = require("express");
 var router = express.Router();
-const FreeLancer=require("../Data-Base/freelancer/freelancer")
+const FreeLancer = require("../Data-Base/freelancer/freelancer");
 
-             /*Login FreeLancer*/
+/*Login FreeLancer*/
 
-router.post('/Login', function(req, res, next) {
-console.log(req.body)
- FreeLancer.loginFreeLancer(req.body,(result,error)=>{
-     if(result.userData){
-         delete result.userData.password
- res.send({Login:true,userData:result.userData})
-     }else{
- res.send({Login:false,userData:result.userData})
-}
- })
-  });
-             /*Signup FreeLancer*/
-
-router.post('/Signup', function(req, res, next) {
-
-    if(Object.keys(req.body).length){
- FreeLancer.SignupFreeLancer(req.body,(result,error)=>{
-  if(error){
-      if(error.code=="ER_DUP_ENTRY"){
-          res.send({Dup:true})
-         }
-    }else{
-          res.send({Signup:true})
-        }
-    })
-}else{
-    res.send({Signup:false})
-}
+router.post("/Login", async function(req, res, next) {
+  console.log(req.body);
+  try {
+    result = await FreeLancer.loginFreeLancer(req.body);
+    if (result.userData) {
+      delete result.userData.password;
+      res.send({ Login: true, userData: result.userData });
+    } else {
+      res.send({ Login: false, userData: result.userData });
+    }
+  } catch (err) {
+    console.log(err);
+  }
 });
- 
-module.exports=router
+/*Signup FreeLancer*/
+
+router.post("/Signup", function(req, res, next) {
+  if (Object.keys(req.body).length) {
+    FreeLancer.SignupFreeLancer(req.body, (result, error) => {
+      if (error) {
+        if (error.code == "ER_DUP_ENTRY") {
+          res.send({ Dup: true });
+        }
+      } else {
+        res.send({ Signup: true });
+      }
+    });
+  } else {
+    res.send({ Signup: false });
+  }
+});
+
+module.exports = router;
 //{FisrtName:'alaa',LastName:'lassoued',Email:'d',Password:'0000',Gender:'hello',Age:21,City:'tunis',Adresse:"mg",Field:"designe"}
